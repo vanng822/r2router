b2cf259eef1c2a18aca99785a373ee9f2988cd6a
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-22 17:04:57 +0100

    Adding first version of router. Seems working but need some more speed

---
+++ router.go
@@ -0,0 +1,126 @@
+package r2router
+
+import (
+	"net/http"
+	"strings"
+	"time"
+	"log"
+)
+
+const (
+	HTTP_METHOD_GET     = "GET"
+	HTTP_METHOD_POST    = "POST"
+	HTTP_METHOD_DELETE  = "DELETE"
+	HTTP_METHOD_OPTIONS = "OPTIONS"
+	HTTP_METHOD_HEAD    = "HEAD"
+	HTTP_METHOD_PUT     = "PUT"
+)
+
+type Params map[string]string
+
+type Handler func(http.ResponseWriter, *http.Request, Params)
+
+type Router struct {
+	roots map[string]*rootNode
+}
+
+func NewRouter() *Router {
+	r := &Router{}
+	r.roots = make(map[string]*rootNode)
+	return r
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	now := time.Now()
+	if root, exist := r.roots[req.Method]; exist {
+		found, handler, params := root.match(req.URL.Path)
+		if found {
+			handler(w, req, params)
+			log.Println(time.Now().Sub(now))
+			return
+		}
+	}
+	
+	http.NotFound(w, req)
+}
+
+func (r *Router) Get(path string, handler Handler) {
+	if _, exists := r.roots[HTTP_METHOD_GET]; !exists {
+		r.roots[HTTP_METHOD_GET] = newRouteTree()
+	}
+	r.roots[HTTP_METHOD_GET].addRoute(path, handler)
+}
+
+func (r *Router) Head(path string, handler Handler) {
+	
+}
+
+func (r *Router) Post(path string, handler Handler) {
+	if _, exists := r.roots[HTTP_METHOD_POST]; !exists {
+		r.roots[HTTP_METHOD_POST] = newRouteTree()
+	}
+	r.roots[HTTP_METHOD_POST].addRoute(path, handler)
+}
+
+func (r *Router) Put(path string, handler Handler) {
+	if _, exists := r.roots[HTTP_METHOD_PUT]; !exists {
+		r.roots[HTTP_METHOD_PUT] = newRouteTree()
+	}
+	r.roots[HTTP_METHOD_PUT].addRoute(path, handler)
+}
+
+func (r *Router) Delete(path string, handler Handler) {
+	if _, exists := r.roots[HTTP_METHOD_DELETE]; !exists {
+		r.roots[HTTP_METHOD_DELETE] = newRouteTree()
+	}
+	r.roots[HTTP_METHOD_DELETE].addRoute(path, handler)
+}
+
+func (r *Router) Options(path string, handler Handler) {
+	
+}
+
+func (r *Router) Group(path string, fn func(r *GroupRouter)) {
+	gr := NewGroupRouter(r, path)
+	fn(gr)
+}
+
+type GroupRouter struct {
+	router *Router
+	path   string
+}
+
+func NewGroupRouter(r *Router, path string) *GroupRouter {
+	gr := &GroupRouter{}
+	gr.router = r
+	gr.path = strings.TrimRight(path, "/")
+	return gr
+}
+
+func (gr *GroupRouter) buildPath(path string) string {
+	return gr.path + "/" + strings.TrimLeft(path, "/")
+}
+
+func (gr *GroupRouter) Get(path string, handler Handler) {
+	gr.router.Get(gr.buildPath(path), handler)
+}
+
+func (gr *GroupRouter) Head(path string, handler Handler) {
+	gr.router.Head(gr.buildPath(path), handler)
+}
+
+func (gr *GroupRouter) Post(path string, handler Handler) {
+	gr.router.Post(gr.buildPath(path), handler)
+}
+
+func (gr *GroupRouter) Put(path string, handler Handler) {
+	gr.router.Put(gr.buildPath(path), handler)
+}
+
+func (gr *GroupRouter) Delete(path string, handler Handler) {
+	gr.router.Delete(gr.buildPath(path), handler)
+}
+
+func (gr *GroupRouter) Options(path string, handler Handler) {
+	gr.router.Options(gr.buildPath(path), handler)
+}
---
+++ routetree.go
@@ -0,0 +1,136 @@
+package r2router
+
+import (
+	//"fmt"
+	"strings"
+)
+
+type routeNode struct {
+	paramNode bool
+	paramName string
+	path      string
+	children  []*routeNode
+	handler   Handler
+}
+
+func newRouteNode() *routeNode {
+	r := &routeNode{}
+	r.children = make([]*routeNode, 0)
+	return r
+}
+
+func (n *routeNode) findChild(nn *routeNode) *routeNode {
+	for _, c := range n.children {
+		if c.paramNode && nn.paramNode {
+			// both are paramNode
+			return c
+		} else if c.path == nn.path {
+			// same node
+			return c
+		}
+	}
+	return nil
+}
+
+func (n *routeNode) swapChild(i, j int) {
+	n.children[i], n.children[j] = n.children[j], n.children[i]
+}
+
+func (n *routeNode) insertChild(nn *routeNode) *routeNode {
+	if child := n.findChild(nn); child != nil {
+		if child.paramNode && nn.paramNode {
+			// only allow one param child, unique param name
+			if child.paramName != nn.paramName {
+				panic("Param name must be same for")
+			}
+		}
+		return child
+	}
+
+	n.children = append(n.children, nn)
+	if len(n.children) > 1 {
+		if n.children[len(n.children)-1].paramNode {
+			n.swapChild(len(n.children)-1, len(n.children))
+		}
+	}
+	return nn
+}
+
+type rootNode struct {
+	root    *routeNode
+	handler Handler
+}
+
+func newRouteTree() *rootNode {
+	r := &rootNode{}
+	r.root = newRouteNode()
+	return r
+}
+
+func (n *rootNode) addRoute(path string, handler Handler) {
+	// /group/:id
+	// /group/:id/action
+	paths := strings.Split(strings.TrimLeft(path, "/"), "/")
+	var parent *routeNode
+	if len(paths) > 1 {
+		parent = n.root
+		for _, p := range paths {
+			//fmt.Println(p)
+			child := newRouteNode()
+
+			if strings.Contains(p, ":") {
+				// param type
+				child.paramName = p[1:]
+				child.paramNode = true
+			} else {
+				child.path = p
+			}
+			parent = parent.insertChild(child)
+		}
+		// adding handler
+		parent.handler = handler
+	} else if path == "/" {
+		if n.handler != nil {
+			panic("There is already a handler for /")
+		}
+		n.handler = handler
+	}
+}
+
+func (n *rootNode) match(path string) (bool, Handler, Params) {
+	paths := strings.Split(strings.TrimLeft(path, "/"), "/")
+	if len(paths) > 1 {
+		var matched bool
+		route := n.root
+		params := make(Params)
+		for _, p := range paths {
+			matched = false
+			for _, c := range route.children {
+				if c.path == p {
+					//fmt.Println("match:", c.path)
+					route = c
+					matched = true
+					break
+				}
+				if c.paramNode {
+					route = c
+					matched = true
+					params[c.paramName] = p
+					break
+				}
+			}
+			if !matched {
+				break
+			}
+		}
+		if matched {
+			return true, route.handler, params
+		}
+	} else if path == "/" {
+		if n.handler != nil {
+			return true, n.handler, nil
+		}
+	}
+
+	return false, nil, nil
+}
