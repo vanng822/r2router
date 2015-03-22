6f351726da65132bd1ab270aff92e1742b1df7c6
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-22 22:54:10 +0100

    Adding test for router

---
+++ router_test.go
@@ -0,0 +1,76 @@
+package r2router
+
+import (
+	"github.com/stretchr/testify/assert"
+	//"github.com/julienschmidt/httprouter"
+	//"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouter(t *testing.T) {
+	router := NewRouter()
+	router.Get("/user/keys", func(w http.ResponseWriter, r *http.Request, p Params) {
+		w.Write([]byte("GET:/user/keys"))
+	})
+	router.Post("/user/keys", func(w http.ResponseWriter, r *http.Request, p Params) {
+		w.Write([]byte("POST:/user/keys"))
+	})
+	router.Put("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, p Params) {
+		w.Write([]byte("PUT:/user/keys/:id," + p["id"]))
+	})
+	router.Delete("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, p Params) {
+		w.Write([]byte("DELETE:/user/keys/:id," + p["id"]))
+	})
+	router.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, p Params) {
+		w.Write([]byte("GET:/user/keys/:id," + p["id"]))
+	})
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	// get
+	res, err := http.Get(ts.URL + "/user/keys/testing")
+	assert.Nil(t, err)
+	content, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, content, []byte("GET:/user/keys/:id,testing"))
+	
+	// Get all
+	res, err = http.Get(ts.URL + "/user/keys")
+	assert.Nil(t, err)
+	content, err = ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	
+	assert.Nil(t, err)
+	assert.Equal(t, content, []byte("GET:/user/keys"))
+	
+	
+	// post
+	res, err = http.Post(ts.URL + "/user/keys", "", nil)
+	assert.Nil(t, err)
+	content, err = ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	
+	assert.Nil(t, err)
+	assert.Equal(t, content, []byte("POST:/user/keys"))
+	
+	// put
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", ts.URL + "/user/keys/testing", nil)
+	res, err = client.Do(req)
+	content, err = ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	assert.Equal(t, content, []byte("PUT:/user/keys/:id,testing"))
+	
+	// delete
+	req, err = http.NewRequest("DELETE", ts.URL + "/user/keys/testing", nil)
+	res, err = client.Do(req)
+	content, err = ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	assert.Equal(t, content, []byte("DELETE:/user/keys/:id,testing"))
+	
+}
