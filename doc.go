8e051a5b71ce033544e7c3852bc7eb3ee60df81f
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-22 17:42:45 +0100

    Adding doc and license

---
+++ doc.go
@@ -0,0 +1,18 @@
+// Package r2router provide a simple router. Suitable for API where you could map parameter from the url.
+//
+//	package main
+// 
+//	import (
+//		"github.com/vanng822/r2router"
+//		"net/http"
+//	)
+//
+//
+//	func main() {
+//		router := r2router.NewRouter()
+// 		router.Get("/users/:user", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
+//			w.Write([]byte(fmt.Sprintf("#v", p)))
+//		})
+//		http.ListenAndServe(":8080", router)
+//	}
+package r2router
