2753b0ae0a6fd6056458fc5d911055bbd40050bd
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-24 23:38:41 +0100

    If middleware not calling next then Written not execute. Go seems to response well.

--- seefor.go
+++ seefor.go
@@ -1,11 +1,11 @@
 package r2router
 
 import (
-	"fmt"
+	//"fmt"
 	"net/http"
 )
 
-type Middleware func(*ResponseWriter, *http.Request, Params, func())
+type Middleware func(http.ResponseWriter, *http.Request, Params, func())
 
 type ResponseWriter struct {
 	status int
@@ -25,7 +25,6 @@ func (w *ResponseWriter) Write(content []byte) (int, error) {
 }
 
 func (w *ResponseWriter) Written() bool {
-	fmt.Println("SDF")
 	return w.status != 0
 }
 
@@ -81,7 +80,7 @@ func (c4 *Seefor) UseHandler(handler Handler) {
 }
 
 func (c4 *Seefor) Wrap(handler Handler) Middleware {
-	return func(w *ResponseWriter, req *http.Request, params Params, next func()) {
+	return func(w http.ResponseWriter, req *http.Request, params Params, next func()) {
 		handler(w, req, params)
 		next()
 	}
--- seefor_test.go
+++ seefor_test.go
@@ -86,7 +86,7 @@ func TestSeeforMiddleware(t *testing.T) {
 	router.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, p Params) {
 		w.Write([]byte("GET:/user/keys/:id," + p["id"] +  p["middleware"]))
 	})
-	router.Use(func(w *ResponseWriter, r *http.Request, p Params, next func()) {
+	router.Use(func(w http.ResponseWriter, r *http.Request, p Params, next func()) {
 		p["middleware"] = "Test Middleware"
 		next()
 	})
@@ -109,7 +109,7 @@ func TestSeeforMultiMiddleware(t *testing.T) {
 	router.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, p Params) {
 		w.Write([]byte("GET:/user/keys/:id," + p["id"] + p["middleware"] + p["hello"]))
 	})
-	router.Use(func(w *ResponseWriter, r *http.Request, p Params, next func()) {
+	router.Use(func(w http.ResponseWriter, r *http.Request, p Params, next func()) {
 		p["middleware"] = "Test Middleware"
 		p["hello"] = "World"
 		next()
@@ -136,11 +136,15 @@ func TestSeeforMiddlewareWritten(t *testing.T) {
 	router.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, p Params) {
 		w.Write([]byte("GET:/user/keys/:id," + p["id"] + p["middleware"] + p["hello"]))
 	})
-	router.Use(func(w *ResponseWriter, r *http.Request, p Params, next func()) {
-		w.WriteHeader(http.StatusNotFound)
+	router.Use(func(w http.ResponseWriter, r *http.Request, p Params, next func()) {
+		w.Write([]byte("Hello"))
+		next()
 	})
-
-
+	
+	router.UseHandler(func(w http.ResponseWriter, r *http.Request, p Params) {
+		p["middleware"] = "Middleware"
+	})
+	
 	ts := httptest.NewServer(router)
 	defer ts.Close()
 
@@ -148,6 +152,6 @@ func TestSeeforMiddlewareWritten(t *testing.T) {
 	assert.Nil(t, err)
 	content, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
-	assert.Equal(t, res.StatusCode, http.StatusNotFound)
-	assert.Equal(t, content, []byte("GET:/user/keys/:id,testingMiddlewareWorld"))
+	assert.Equal(t, res.StatusCode, http.StatusOK)
+	assert.Equal(t, content, []byte("Hello"))
 }
