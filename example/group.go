4f9ff164b2d4c6987fa3fdeebd474b8bc4a0a360
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-27 19:37:58 +0100

    Using unrolled render in example to render json

--- example/group.go
+++ example/group.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/unrolled/render"
 	"github.com/vanng822/r2router"
 	"net/http"
 )
 
 func main() {
+	renderer := render.New()
 	router := r2router.NewRouter()
 	router.Group("/repos/:owner/:repo", func(r *r2router.GroupRouter) {
 		r.Get("/stats/contributors", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
-			w.Write([]byte(fmt.Sprintf("#v", p)))
+			data := map[string]string{}
+			data["owner"] = p.Get("owner")
+			data["repo"] = p.Get("repo")
+			renderer.JSON(w, http.StatusOK, data)
 		})
-		
+
 		r.Get("/releases/:id/assets", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
 			w.Write([]byte(fmt.Sprintf("#v", p)))
 		})
-		
+
 		r.Get("/issue", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
 			w.Write([]byte(fmt.Sprintf("#v", p)))
 		})
 	})
-	
+
 	http.ListenAndServe(":8080", router)
 }
