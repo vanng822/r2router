5abc3a47b9d4eef32ed1277466d1105b5b013a2a
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-28 18:10:24 +0100

    Adding timer example

---
+++ example/timer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/vanng822/r2router"
+	"net/http"
+)
+
+func main() {
+	router := r2router.NewSeeforRouter()
+	router.Group("/hello", func(r *r2router.GroupRouter) {
+		r.Get("/kitty", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
+			w.Write([]byte("Mau"))
+		})
+
+		r.Get("/duck", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
+			w.Write([]byte("Crispy"))
+		})
+
+		r.Get("/:name", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
+			w.Write([]byte(p.Get("name")))
+		})
+	})
+	timer := router.UserTimer(nil)
+	
+	go http.ListenAndServe("127.0.0.1:8080", router)
+	http.ListenAndServe("127.0.0.1:8081", timer)
+}
