8e89fdaa0a78ab51010f87cca2453c33ad79b841
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-03-30 20:56:01 +0200

    Update example after changes

--- example/middleware.go
+++ example/middleware.go
@@ -10,17 +10,18 @@ import (
 
 func main() {
 	seefor := r2router.NewSeeforRouter()
+	
 	// measure time middleware
-	seefor.Before(func(w http.ResponseWriter, r *http.Request, next func()) {
+	seefor.Before(r2router.BeforeFunc(func(w http.ResponseWriter, r *http.Request, next func()) {
 		start := time.Now()
 		next()
 		log.Printf("took: %s", time.Now().Sub(start)) 
-	})
+	}))
 	// set label "say"
-	seefor.After(func(w http.ResponseWriter, r *http.Request, p r2router.Params, next func()) {
+	seefor.After(r2router.AfterFunc(func(w http.ResponseWriter, r *http.Request, p r2router.Params, next func()) {
 		p.AppSet("say", "Hello")
 		next()
-	})
+	}))
 	seefor.Get("/hello/:name", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
 		w.Write([]byte(fmt.Sprintf("%s %s!", p.AppGet("say").(string), p.Get("name"))))
 	})
