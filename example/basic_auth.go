799937015579905e3087d2ca564840d4785bf24f
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-04 15:13:50 +0200

    Basic auth example

---
+++ example/basic_auth.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/goji/httpauth"
+	"github.com/vanng822/r2router"
+	"net/http"
+)
+
+// Example how to use basic auth together with r2router
+func main() {
+	router := r2router.NewSeeforRouter()
+	
+	// basic auth for entire route
+	http.Handle("/", httpauth.SimpleBasicAuth("testuser", "testpw")(router))
+	
+	router.Get("/hello/:name", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
+		w.Write([]byte( p.Get("name")))
+	})
+
+	http.ListenAndServe("127.0.0.1:8080", nil)
+}
