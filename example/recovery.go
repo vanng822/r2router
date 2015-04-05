f816f65b1b7fac096ff3f2ddf1519be85b557368
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-05 22:25:00 +0200

    gofmt

--- example/recovery.go
+++ example/recovery.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "github.com/vanng822/r2router"
-    "github.com/vanng822/recovery"
-    "net/http"
+	"github.com/vanng822/r2router"
+	"github.com/vanng822/recovery"
+	"net/http"
 )
 
 func main() {
-    seefor := r2router.NewSeeforRouter()
-    rec := recovery.NewRecovery()
-    rec.PrintStack = true
-    seefor.Before(rec.Handler)
+	seefor := r2router.NewSeeforRouter()
+	rec := recovery.NewRecovery()
+	rec.PrintStack = true
+	seefor.Before(rec.Handler)
 	seefor.Before(r2router.WrapBeforeHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		panic("Middleware panic")
 	})))
-	
-    http.ListenAndServe(":8080", seefor)
+
+	http.ListenAndServe(":8080", seefor)
 }
