1e422259c80157424e6f624ca3e9e9c176bf63fe
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-07 19:53:45 +0200

    Should use double :

--- example/routemanager.go
+++ example/routemanager.go
@@ -12,16 +12,16 @@ func main() {
 	seefor := r2router.NewSeeforRouter()
 	rmanager := r2router.NewRouteManager()
 	// register and use it at the same time
-	seefor.Get(rmanager.Add("hello:name", "/hello/:name"), func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
+	seefor.Get(rmanager.Add("hello::name", "/hello/:name"), func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
 		fmt.Fprintf(w, "Hello %s!", p.Get("name"))
 	})
 
 	// Or register first elsewhere and get it later
-	rmanager.Add("redirect:name", "/redirect/:name")
+	rmanager.Add("redirect::name", "/redirect/:name")
 
-	seefor.Get(rmanager.PathFor("redirect:name"), func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
+	seefor.Get(rmanager.PathFor("redirect::name"), func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
 		// Building url for routename "hello:name" and redirect
-		http.Redirect(w, r, rmanager.UrlFor("hello:name", MM{"name": []string{p.Get("name")}}), http.StatusFound)
+		http.Redirect(w, r, rmanager.UrlFor("hello::name", MM{"name": []string{p.Get("name")}}), http.StatusFound)
 	})
 
 	http.ListenAndServe("127.0.0.1:8080", seefor)
